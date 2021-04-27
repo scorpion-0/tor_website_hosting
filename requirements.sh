7b4946b17e276b30453bd9ca6a078f2f92401e6e
scorpion-0 <76430265+scorpion-0@users.noreply.github.com>
2021-04-27 11:16:06 +0300

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,6 @@
+echo "$(tput sgr 1) Made by scorp1on-0$(tput sgr 0)"
+sleep 1
+echo "$(tput sgr 1) Please consider staring the project$(tput sgr 0)"
 echo "Installing requirements..."
 sudo apt install tor
 sudo apt install python3
