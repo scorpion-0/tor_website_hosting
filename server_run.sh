de70d597e6cd323319a2bf0860b9f32e2aa915a6
scorpion-0 <76430265+scorpion-0@users.noreply.github.com>
2021-04-27 14:41:40 +0300

    Create server_run.sh

--- server_run.sh
+++ server_run.sh
@@ -6,32 +6,53 @@ echo "|_   _/ _ \|  _ \  | | | |/ _ \/ ___|_   _|_ _| \ | |/ ___|"
 echo "  | || | | | |_) | | |_| | | | \___ \ | |  | ||  \| | |  _ "
 echo "  | || |_| |  _ <  |  _  | |_| |___) || |  | || |\  | |_| |"
 echo "  |_| \___/|_| \_\ |_| |_|\___/|____/ |_| |___|_| \_|\____|$(tput setaf 2)"
-sleep 1
+sleep 0.3
 echo " "
-echo "$(tput setaf 3) This tool was made by scorp1on-0$(tput setaf 0)"
+echo "$(tput sgr 1)$(tput setaf 2) This tool was made by scorp1on-0$(tput setaf 0))$(tput sgr 0)"
 echo " "
-sleep 1
+sleep 2
 sudo echo "Checking..."
 echo "$(tput setaf 1)DISCLAMER: I AM NOT RESPONSABLE FOR WHAT YOU DO WITH THIS TOOL. PLEASE DO NOT USE IT IN ILEGAL PURPOSE.$(tput sgr 0)"
+sleep 2
 #Now starting
 echo "Now starting service..."
+sleep 0.1
 echo "Stand by"
+sleep 0.2
 #After you configure the file, you can take this warning out. (If you dont know what file to configure, read the README.mf file)
 echo "$(tput setaf 1)WARNING: Your system may NOT configured by default, read the README.md file for instructions.$(tput sgr 0)"
+sleep 0.2
 echo "Starting Tor..."
+sleep 0.2
 echo "Tor will run in the background"
 #Starting tor in the background
+sleep 0.2
+echo "$(tput setaf 0) Tor is starting..."
 sudo tor &
+echo "Tor started, moving on"
 #Printing the domain name
-echo "$(tput setaf 5)==================================================================="
-echo "This is the hostname of your .onion website: (NOTE: The hostname can change after you restart the service, it is located in /var/lib/hidden_service/hostname)"
+sleep 0.2
+echo "$(tput setaf 2)==================================================================="
+echo "This is the hostname of your .onion website: (NOTE: The hostname can change after you restart the service, it is located in /var/lib/hidden_service/hostname)$(tput sgr 1) "
+echo "| HOSTNAME |----------------------------------------------------------------------|"
 sudo cat /var/lib/tor/hidden_service/hostname
-echo "===================================================================$(tput setaf 2)"
+echo "| HOSTNAME |----------------------------------------------------------------------|"
+sleep 1
+echo "$(tput sgr 0)==================================================================="
+sleep 0.2
+echo "$(tput sgr 1)THIS IS THE HOSTNAME, paste this in the Tor Browser$(tput sgr 0)"
 echo "Starting python server..."
-echo "To stop the server, hit CTRL + C"
+sleep 0.2
+
+echo "$(tput setaf 0)To stop the server, hit CTRL + C"
+sleep 2
 #Starting python3 in the background
 python3 -m http.server --bind 127.0.0.1 8080 &
-echo "Python3 is now running in the background"
+sleep 0.2
+echo "Python3 is now running in the background$(tput setaf 2)"
+sleep 0.2
 #You are done here 
+echo "|-------------------------------------------|"
 echo "Your site is up and running!"
 echo "To stop the service, run the stop.sh file"
+sleep 0.5
